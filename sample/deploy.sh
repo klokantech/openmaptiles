6fefabbb00739cdd3f14a8ba5f4fce81de8689d4
jirik <jirik@users.noreply.github.com>
2017-10-19 21:40:44 +0200

    Sample: Make travis logs shorter

--- sample/deploy.sh
+++ sample/deploy.sh
@@ -11,16 +11,15 @@ cp ./sample/tile.json ./data/
 rm -rf ./data/tiles
 git clone https://github.com/mapbox/mbutil.git
 cd data
-./../mbutil/mb-util --image_format=pbf tiles.mbtiles tiles
+./../mbutil/mb-util --image_format=pbf --silent tiles.mbtiles tiles
 cd tiles
-rm -f ./metadata.json
 gzip -d -r -S .pbf *
-find . -type f -exec mv '{}' '{}'.pbf \;
+find . -type f ! -name 'metadata.json' -exec mv '{}' '{}'.pbf \;
 cd ..
 git init
 git add tiles.mbtiles
 git add quickstart.log
 git add tile.json
 git add tiles/**/*
-git commit -m "Deploy to Github Pages"
+git commit --quiet -m "Deploy to Github Pages"
 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1
